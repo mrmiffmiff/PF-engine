c05ec7ed7caa274dfdea7dfba1775b320bd98431
Robert Eisenman <robert.t.eisenman@gmail.com>
2019-04-07 11:12:02 -0700

    Whitespace removal

--- PathfinderEngine/PathfinderEngine/Program.cs
+++ PathfinderEngine/PathfinderEngine/Program.cs
@@ -22,12 +22,21 @@ namespace PathfinderEngine
         }
     }
 
+    public static class StatMethods
+    {
+        public static string RemoveWhitespace(this string str)
+        {
+            return string.Join("", str.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             while (true) {
                 string dieCode = Console.ReadLine();
+                dieCode = StatMethods.RemoveWhitespace(dieCode);
                 string[] splitCode = dieCode.Split('d');
                 try { if (Int32.Parse(splitCode[0]) == 0) break; }
                 catch { System.Environment.Exit(0); }
